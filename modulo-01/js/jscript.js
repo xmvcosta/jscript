e1cac426033b930d100c337bd6684fcc68fc44e6
Marcos-TTI <xmvcosta@gmail.com>
2022-05-05 17:23:46 -0300

    primeiro commit

---
+++ modulo-01/js/jscript.js
@@ -0,0 +1,28 @@
+//Modulo 01 Aula 04
+//https://alunos.b7web.com.br/curso/javascript/formas-de-mostrar-output
+
+// document.getElementById("titulo").innerHTML = "Novo titulo"
+// document.write("Algum texto")
+
+// window.alert("mostrar na janela")
+
+// console.log("teste123")
+
+////////////////////////////////////////////////////////////////////////////
+//Modulo 01 Aula 05
+//https://alunos.b7web.com.br/curso/javascript/variaveis-1
+
+//variaveis nao pode ter espaço, caracter espercial, nao pode começar com um numero
+// var idade = 38
+// var nome = "Marcos" //string
+// var sobreNome = "Costa" //string
+// var x = 10
+// var y = 15
+// total = x + y
+// document.write(nome + " " + sobreNome + " O valor total é: " + total)
+// window.alert(nome + " " + sobreNome + " O valor total é: " + total)
+
+
+////////////////////////////////////////////////////////////////////////////
+//Modulo 01 Aula 06
+//https://alunos.b7web.com.br/curso/javascript/condicionais-1
