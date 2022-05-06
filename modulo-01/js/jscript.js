5caf2792cdfb12c1b18b16ad6700104e784d47f9
Marcos Costa <xmvcosta@gmail.com>
2022-05-05 21:18:14 -0300

    reorganizei a pasta

--- modulo-01/js/jscript.js
+++ modulo-01/js/jscript.js
@@ -37,3 +37,78 @@
 //     document.write("não é de manhã")
 // }
 
+
+////////////////////////////////////////////////////////////////////////////
+//  Modulo 01 Aula 07
+//  https://alunos.b7web.com.br/curso/javascript/condicionais-2
+//
+
+// var hora = 19
+
+// if (hora < 12){
+//     console.log("Bom dia!")
+// } else if (hora < 18){
+//     console.log("Boa tarde!")
+// } else if (hora <=23){
+//     console.log("Boa noite!")
+// }
+
+// var hora = 12
+// if (hora >= 12 && hora < 18 ){
+//     console.log("Boa tarde!")
+// }
+
+// var hora = 19
+// if ( hora == 12 || hora == 18 ){
+//     console.log("Você está no horário de RUSH!")
+//     document.getElementById("titulo").innerHTML = "Você está no horário de RUSH!"
+
+// }
+
+////////////////////////////////////////////////////////////////////////////
+//  Modulo 01 Aula 08
+//  https://alunos.b7web.com.br/curso/javascript/variaveis-2
+
+// var nome = "Marcos"
+
+// let nome = "Marcos"
+
+// const nome = "Pedro"
+
+// var nome = "Marcos"
+
+// if( nome == "Marcos"){
+//     var idade = 38
+// } else { console.log("Não é o Marcos")}
+
+// console.log("Marcos tem " + idade + " anos.")
+
+// let nome = "Marcos" //define a variavel e atrui um valor a ela.
+// nome = "Pedro" //redefine o valor da variavel sem a necessidade de declaraá-la novamente.
+
+// if( nome == "Pedro"){
+//     let idade = 38
+//     console.log(idade)
+// }
+
+//console.log(idade) 
+
+// const nome = "Marcos"
+// nome = "Pedro"
+// if ( nome == "Marcos"){
+
+//     console.log(idade)
+
+// }
+
+////////////////////////////////////////////////////////////////////////////
+//  Modulo 02 Aula 02
+//  https://alunos.b7web.com.br/curso/javascript/selecionando-elementos-4-funcoes-1
+//document.getElementById("exemplo").innerHTML = "Teste"
+//document.getElementById("exemplo").innerHTML = "<p style = color:blue>Teste</p>"
+
+////////////////////////////////////////////////////////////////////////////
+//  Modulo 02 Aula 02
+//  https://alunos.b7web.com.br/curso/javascript/selecionando-elementos-4-funcoes-2
+
+document.getElementsByClassName("lista")[0].innerHTML = ""
