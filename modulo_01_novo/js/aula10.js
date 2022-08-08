efcb7370a19fbf1f155b1fb7527f82ee605ca404
Marcos Casa <xmvcosta@gmail.com>
2022-08-08 01:04:34 -0300

    diversas alterações de nome de pastas e novos codigos

---
+++ modulo_01_novo/js/aula10.js
@@ -0,0 +1,3 @@
+let nome = "Marcos";
+console.log(nome);
+document.getElementById("paragrafo").innerHTML = "<b>Algum texto aqui</b>"
---
+++ modulo_01_novo/js/aula11.js
@@ -0,0 +1,5 @@
+let nome = "Marcos";
+let sobreNome = "Costa";
+let nomeCompleto = nome + ' '+sobreNome;
+document.getElementById("titulo").innerHTML = nomeCompleto;
+console.log(nomeCompleto);
