e5e3379dbeb683a8d3a4390110581f0cb2c51bdf
Marcos Casa <xmvcosta@gmail.com>
2022-08-24 03:35:30 -0300

    alterações feitas em casa 03:35 24/08/2022

--- programacao_web/js/script.js
+++ programacao_web/js/script.js
@@ -1,22 +1,7 @@
-
-
-/*
-function soma(valor1, valor2){
-    return valor1 + valor2;
-   
-
-}
-//document.getElementById("texto").innerHTML = soma(15, 20);
-
-function realParaDolar(real, cotacaoDolar){
-    return real * cotacaoDolar;
-}
-var total = realParaDolar(10, 5.08);
-alert (total);
-*/
-
-//https://www.youtube.com/watch?v=McKNP3g6VBA&t=7990s&ab_channel=Programa%C3%A7%C3%A3oWeb
-//objetos
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                              OBJETOS                                              //
+//  https://www.youtube.com/watch?v=McKNP3g6VBA&t=7990s&ab_channel=Programa%C3%A7%C3%A3oWeb         //
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
 const carro = {
     marca: "Ford",
@@ -29,49 +14,59 @@ const carro = {
 };
 */
 
-
-//https://www.youtube.com/watch?v=McKNP3g6VBA&t=7990s&ab_channel=Programa%C3%A7%C3%A3oWeb
-//eventos
-
-function eventoClique(){
-    //alert("Clicou");
-    console.log("Clicou");
-    document.body.style.backgroundColor = "yellow";
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                              FUNÇÔES                                             //
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+function eventoClique() {
+  //alert("Clicou");
+  console.log("Clicou");
+  document.body.style.backgroundColor = "yellow";
 }
 
-function corVermelha(){
-    let cor = document.getElementById("teste");
-    cor.style.backgroundColor = "green";
+function corVermelha() {
+  let cor = document.getElementById("teste");
+  cor.style.backgroundColor = "green";
 }
 
-function corAzul(){
-    let cor = document.getElementById("teste");
-    cor.style.backgroundColor = "blue";
+function corAzul() {
+  let cor = document.getElementById("teste");
+  cor.style.backgroundColor = "blue";
 }
 
-//const pessoa = ["Marcos","Costa",38]; //array
-//const pessoa = {nome:"Marcos", sobreNome:"Costa", idade:38};//objeto
-
-//const lista = ["Arroz", "Feijão", "Macarrão","Pão"]
-//console.log(lista);
-
-/*
-const pessoa = ["Marcos","Costa",38,"Técnico" ];
-//pessoa.push("brasileiro");
-//console.log(pessoa[pessoa.length -1]);
-//alert(Array.isArray(pessoa)); //para saber se é um array
-
-document.getElementById("texto").innerHTML = pessoa.join(" * ");
-*/
-
-
-function limparTexto(){
-    document.getElementById("nome").value = "";
+function limparTexto() {
+  document.getElementById("nome").value = "";
 }
 
-function mudou(){
-    console.log("mudou");
+function mudou() {
+  console.log("mudou");
 }
 
+function digitou() {
+  let digitou = document.getElementById("nome").value;
+  console.log(digitou);
+}
+
+function realParaDolar(real, cotacaoDolar) {
+  return real * cotacaoDolar;
+}
+function soma(valor1, valor2) {
+  return valor1 + valor2;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                              ARRAYS                                              //
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//const lista = ["arroz","feijão","macarrão","carne"];
+//let t = document.getElementById("texto");
+//t.innerText = lista;
+//t.innerHTML = lista;
+//t.innerHTML = lista[1];
+
+const pessoa = ["Marcos", "Costa", 38, "Brasileiro"];
+//pessoa.push("Solteiro"); //adiciona +1 novo valor ao final do array
+//pessoa[pessoa.length] = "Solteiro"; //adiciona +1 novo valor ao final do array
+//alert(pessoa[pessoa.length -1]); //exibe o último item do array 
+console.log(pessoa);
 
 
