498840fb17e95339c882b3b2121bc888ce40dc6e
Marcos Casa <xmvcosta@gmail.com>
2022-08-26 00:26:01 -0300

    alteraçoes nas estruturas das pastas feita em casa 26/08/2022 00:25

--- curso_js_modulo2/string_avancado/scripts.js
+++
@@ -1,4 +0,0 @@
-console.log("Primeira linha \n Segunda linha"); // \n permite quebra de linha
-console.log("O meu nome é 'Marcos'");
-console.log('O nome dela é "Gabriela"');
-console.log(`3 x 5 = ${3*5}`);
