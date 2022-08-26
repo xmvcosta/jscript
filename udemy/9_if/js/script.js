d9046431ddaca905542e4b82139d4054201c31d2
Marcos Casa <xmvcosta@gmail.com>
2022-08-26 14:26:10 -0300

    aula de if em casa 26082022 14:26

---
+++ udemy/9_if/js/script.js
@@ -0,0 +1,28 @@
+// console.log("olá mundo!!!");
+//let maiorNumero = Math.max(1,2,2022,22,38,48); //método Math.max exibe o maior numero
+// console.log(maiorNumero);
+
+// let menorNumero = Math.min(0,2,4,56,78); //metodo Math.min exibe o menor numero;
+// console.log(menorNumero);
+
+// let arredondar = Math.round(38.56789); //arrendonda numeros
+// alert(arredondar);
+
+//  let arredondar = Math.ceil(38.56789); //arrendonda numeros para cima
+//  alert(arredondar);
+
+// let idade = 17;
+
+// if(idade > 18){
+    // console.log("Pode entrar!");
+// } else{
+    // console.log("Não pode entrar");
+// }
+
+let idade = 38;
+let nome = "Marcos";
+let passaPorte = true;
+
+if((idade < 37 && nome == "marcos") || passaPorte == true){
+    console.log("Bem vindo Marcos, liberado a sua entrada.");
+}
---
+++ udemy/funcoes_math/js/script.js
@@ -0,0 +1,13 @@
+// console.log("olá mundo!!!");
+//let maiorNumero = Math.max(1,2,2022,22,38,48); //método Math.max exibe o maior numero
+// console.log(maiorNumero);
+
+// let menorNumero = Math.min(0,2,4,56,78); //metodo Math.min exibe o menor numero;
+// console.log(menorNumero);
+
+// let arredondar = Math.round(38.56789); //arrendonda numeros
+// alert(arredondar);
+
+//  let arredondar = Math.ceil(38.56789); //arrendonda numeros para cima
+//  alert(arredondar);
+
