1e2f7123dd9a881d77c0231f72ab70dd2bbfb240
Marcos Casa <xmvcosta@gmail.com>
2022-08-28 23:07:41 -0300

    estudos em casa 23:10 28/08/2022

--- udemy/continue/js/script.js
+++ udemy/continue/js/script.js
@@ -10,3 +10,105 @@
 //     console.log(i);
 
 // }
+
+// let nome = "Marcos";
+// let idade = 38;
+// let solteiro = true;
+
+// console.log(typeof nome);
+// console.log(typeof idade);
+// console.log(typeof solteiro);
+
+// let idade = 17;
+
+// if(idade >= 18){
+//     console.log("pode entrar na balada");
+// }else {
+//     console.log("Menor de 18 anos. Não pode entrar");
+// }
+
+// const nome = "Marcos";
+// if(nome == "Marcos"){
+//     console.log("Bem vindo Marcos");
+// }
+
+//minha solução
+// let i = Math.pow(3,2);
+// console.log(i);
+
+//solução do professor
+// let num = 18;
+// console.log(Math.pow(2,2));
+// console.log(Math.pow(3,2));
+// console.log(Math.pow(num,2));
+
+// exercicio 10 velocidade
+//minha solução
+// let velocidade = 880;
+
+// if(velocidade <= 80){
+//     console.log("Dentro do limite de velocidade");
+// } else {
+//     console.log("Multado. Você está acima do limite de velocidade!!!");
+// }
+
+
+// exercicio 11
+// minha solução
+// let idade = 19;
+// let cnh = true;
+
+// if(idade > 18 && cnh == true){
+//     console.log("Pode dirigir. Maior de 18 anos com CNH.");
+// } else if(idade > 18 && cnh == false){
+//     console.log("Proibido Dirigir, maior de 18 anos não habilitado");
+
+// } else {
+//     console.log("Menor de 18 anos. Probido dirigir.");
+// }
+
+// exercicio 12
+// minha solução
+// i = 0;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+// exercicio 13
+// minha solução
+// for(let i = 100; i >= 50; i--){
+//     console.log(i);
+// }
+
+// exercicio 13
+// minha solução
+
+// let n = 0;
+
+// while(n <= 50){
+//     //console.log(n);
+//     if(n % 2 == 0){
+//         console.log(`${n} é par`);
+//     } else{
+//         console.log(n + " é impar");
+//     }
+//     n++;
+// }
+
+// function imprimirNoConsole(){
+//     console.log("Função imprimir no console");
+// }
+// imprimirNoConsole();
+
+//arrow function
+const consoleTeste =  () =>{
+    console.log("Olá mundo!");
+}
+consoleTeste();
+//
+//
+let soma = (a,b)=>{
+    return console.log(a + b * 1000);
+}
+soma(5,10);
