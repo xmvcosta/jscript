a74bb4817c1e9a1336ed2c0126e46af82f1ad956
Marcos-TTI <xmvcosta@gmail.com>
2022-08-29 18:05:52 -0300

    alteracoes feitas na firma 290822 18:05

---
+++ projetos_js/js/todolist.js
@@ -0,0 +1,68 @@
+let contador = 0;
+let input = document.getElementById("inputTarefa");
+let btnAdd = document.getElementById("btn-add");
+let main = document.getElementById("areaLista");
+
+function addTarefa() {
+    //pegar valor digitado no input
+    let valorInput = input.value;
+    if ((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)) {
+
+        ++contador;
+
+        let novoItem = `<div id="${contador}" class="item">
+        <div onclick="marcarTarefa(${contador})" class="item-icone">
+            <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>
+        </div>
+        <div onclick="marcarTarefa(${contador})" class="item-nome">
+            ${valorInput}
+        </div>
+        <div class="item-botao">
+            <button onclick="deletar(${contador})" class="delete">
+                <i class="mdi mdi-delete">Deletar</i>
+            </button>
+        </div>
+      </div>`;
+        //adicionar novo item no main
+        main.innerHTML += novoItem;
+
+        //zerar os campos
+        input.value="";
+        input.focus();
+        console.log(contador);
+    }
+}
+
+input.addEventListener("keyup",function(event){
+    if (event.keyCode === 13){
+        event.preventDefault();
+        btnAdd.click();
+    }
+})
+
+function deletar(id){
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+
+function marcarTarefa(id){
+    var item = document.getElementById(id);
+    var classe = item.getAttribute('class');
+    //console.log(classe);
+
+    if( classe == "item"){
+        item.classList.add('clicado');
+
+        var icone = document.getElementById('icone_'+id)
+        icone.classList.remove('mdi-check-circle');
+        icone.classList.add('mdi-circle-outline');
+
+        item.parentNode.appendChild(item);
+    }else{
+        item.classList.remove('clicado');
+
+        var icone = document.getElementById('icone_'+id)
+        icone.classList.remove('mdi-circle-outline');
+        icone.classList.add('mdi-check-circle');
+    }
+}
---
+++ udemy/set_timeout_set_interval/js/script.js
@@ -0,0 +1,139 @@
+// for(let i = 10; i>0; i = i-1){
+//     if(i % 2 == 0){
+//         console.log("caiu no continue par");
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// for (i=0; i<10; i++){
+//     console.log(i);
+
+// }
+
+// let nome = "Marcos";
+// let idade = 38;
+// let solteiro = true;
+
+// console.log(typeof nome);
+// console.log(typeof idade);
+// console.log(typeof solteiro);
+
+// let idade = 17;
+
+// if(idade >= 18){
+//     console.log("pode entrar na balada");
+// }else {
+//     console.log("Menor de 18 anos. Não pode entrar");
+// }
+
+// const nome = "Marcos";
+// if(nome == "Marcos"){
+//     console.log("Bem vindo Marcos");
+// }
+
+//minha solução
+// let i = Math.pow(3,2);
+// console.log(i);
+
+//solução do professor
+// let num = 18;
+// console.log(Math.pow(2,2));
+// console.log(Math.pow(3,2));
+// console.log(Math.pow(num,2));
+
+// exercicio 10 velocidade
+//minha solução
+// let velocidade = 880;
+
+// if(velocidade <= 80){
+//     console.log("Dentro do limite de velocidade");
+// } else {
+//     console.log("Multado. Você está acima do limite de velocidade!!!");
+// }
+
+
+// exercicio 11
+// minha solução
+// let idade = 19;
+// let cnh = true;
+
+// if(idade > 18 && cnh == true){
+//     console.log("Pode dirigir. Maior de 18 anos com CNH.");
+// } else if(idade > 18 && cnh == false){
+//     console.log("Proibido Dirigir, maior de 18 anos não habilitado");
+
+// } else {
+//     console.log("Menor de 18 anos. Probido dirigir.");
+// }
+
+// exercicio 12
+// minha solução
+// i = 0;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+// exercicio 13
+// minha solução
+// for(let i = 100; i >= 50; i--){
+//     console.log(i);
+// }
+
+// exercicio 13
+// minha solução
+
+// let n = 0;
+
+// while(n <= 50){
+//     //console.log(n);
+//     if(n % 2 == 0){
+//         console.log(`${n} é par`);
+//     } else{
+//         console.log(n + " é impar");
+//     }
+//     n++;
+// }
+
+// function imprimirNoConsole(){
+//     console.log("Função imprimir no console");
+// }
+// imprimirNoConsole();
+
+//arrow function
+const consoleTeste =  () =>{
+    console.log("Olá mundo!");
+}
+// consoleTeste();
+// //
+// //
+// let soma = (a,b)=>{
+//     return console.log(a + b * 1000);
+// }
+// soma(5,10);
+
+// setTimeout(consoleTeste,10000);
+// setInterval(consoleTeste,10000);
+
+//classes no javascript
+// class Carro{
+//     constructor(valor1,valor2,valor3){
+//         this.marca = valor1;
+//         this.modelo = valor2;
+//         this.ano = valor3;
+//     }
+// }
+
+// const uno = new Carro("Lamborguini","Diablo",2023);
+// console.log(uno);
+
+//datas no javascript
+// let data = new Date();
+// console.log(data);
+
+// let ano = data.getFullYear();
+// console.log(ano);
+
+// let mes = data.getMonth();
+// console.log(mes+1);
