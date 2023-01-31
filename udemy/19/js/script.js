c7121a2001c8dfecbfad934acb14fb5298a6079d
Marcos Costa <xmvcosta@gmail.com>
2023-01-30 22:52:40 -0300

    udemy javascript 19 special numbers (casa)

---
+++ udemy/19/js/script.js
@@ -0,0 +1,12 @@
+/* 18 Numbers */
+// console.log(typeof (2 + 2));
+// console.log(typeof (2 * 3 + 1));
+// console.log(typeof (5-22));
+// console.log(typeof(4*4));
+// console.log(typeof(20%2));
+// console.log(typeof(21%2));
+
+/* 19. special Numbers */
+console.log(Infinity);
+console.log(-Infinity);
+console.log(NaN);
