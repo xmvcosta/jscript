3d353d51b6f9e6e3d706aae24a5fa7b0781f160b
Marcos TTI <xmvcosta@gmail.com>
2023-05-11 15:43:59 -0300

    alterações feitas na firma 11052023 15:43

---
+++ cfbcursos/javascript/aula07/js/jscript.js
@@ -0,0 +1,24 @@
+"use restrict"
+
+// && -> and else
+// || -> or ou
+// ! -> not não
+
+let n1,n2,n3,n4
+n1=10
+n2=5
+n3=15
+n4=2
+
+// if(n1 > n3){
+//     console.log("Número 1 é maior que o Número 2")
+// }else{
+//     console.log("Número 1 é menor que Número 3")
+//     }
+
+if((n1>n2) && (n3<n4)){
+    console.log("verdadeiro")
+}else{
+    console.log("falso")
+}
+
