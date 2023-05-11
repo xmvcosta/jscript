10e66d6ad256a0fa6385a5b05b64b9c9fb6e5cf8
Marcos Casa <xmvcosta@gmail.com>
2023-05-11 00:04:30 -0300

    alteraçoes em casa 11052023 00:04

---
+++ cfbcursos/javascript/aula04/js/jscript.js
@@ -0,0 +1,59 @@
+"use restrict"
+
+
+//let nome="Marcos";
+//nome = "Costa"
+//nome = 39
+
+//const nome = "Marcos"
+//nome = "Pedro"
+//console.log(nome);
+
+/* Aprenda sobre os Operadores Matemáticos em Javascript - Curso de Javascript Moderno - Aula 04 */
+/*  
+
++
+-
+*
+/
+%
+++
+--
++=
+-=
+
+*/
+
+
+/* 
+let num1 = 0;
+let num2 = 2;
+*/
+
+/*let num1=09, num2 = 10
+res=num1+num2*7
+console.log(res)*/
+
+// let num1 = 20
+
+// num1++
+// num1++
+// num1++
+// num1++
+// num1++
+
+// console.log(num1)
+
+// let num1 = 0
+// num1+=1 //é o mesmo que num1++ ou num1=num1+1
+// console.log(num1)
+
+// let num1=0
+// num1+=5
+// num1+=5
+// num1+=5
+// console.log(num1)
+let num1=10
+num1*=7
+console.log(num1)
+
