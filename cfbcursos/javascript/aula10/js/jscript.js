0dea36a3e01360377b9159e7f5302e1630d7fb7b
Marcos Casa <xmvcosta@gmail.com>
2023-05-11 22:28:01 -0300

    alteraçoes em casa 11052023 22:27

---
+++ cfbcursos/javascript/aula10/js/jscript.js
@@ -0,0 +1,38 @@
+"use restrict"
+//Operador Ternário 
+
+// let n1 = 284
+// let res = n1%2
+
+// if(res == 0){
+//     console.log("Par")
+// }else {
+//     console.log("Impar")
+// }
+
+// let n1 = 1984
+// let res = (!(n1%2) ? "Par":"Impar") // true:false
+// console.log(res)
+
+// true:false
+
+// 0 = false
+// 1 = true
+
+// Teste lógico ? se verdadeiro : se falso
+
+// let n1 = 5
+// let n2 = 10
+
+// let res = ( n1 >= n2 ? "Verdadeiro" : "Falso")
+// console.log(res)
+
+// let st = "J"
+
+// let res = ( st == "A" ? "ATIVO" : "INATIVO" )
+// console.log(res)
+
+
+let st = "B"
+let res = (st == "A" ? "Aluno Ativo" : "Aluno Inativo")
+console.log(res)
---
+++ cfbcursos/javascript/aula12/js/jscript.js
@@ -0,0 +1,12 @@
+"use restrict"
+
+// Operador Spread, aprendendo sobre o espalhador em Javascript - Curso de Javascript Moderno - Aula 12
+
+let n1 = [11,22,33,44,55]
+let n2 = [21,31,41,51]
+let n3
+
+console.log("n1 = " + n1)
+console.log("n2 = " + n2)
+console.log("n3 = " + n3)
+
