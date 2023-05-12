f39607141e1d68e64ff9fd65ccb6bd992a6f20c4
Marcos Casa <xmvcosta@gmail.com>
2023-05-11 21:07:03 -0300

    alteraçoes em casa 11052023 21:06

---
+++ cfbcursos/javascript/aula08/js/jscript.js
@@ -0,0 +1,18 @@
+"use restrict"
+
+//operadores bitwise "bit a bit"
+
+// let n1=10
+// let n2=11
+// let res = n1 & n2
+
+// console.log(res)
+
+// let n1=20
+
+// let res = n1 >> 2
+// console.log(res)
+
+
+
+
---
+++ cfbcursos/javascript/aula09/js/jscript.js
@@ -0,0 +1,12 @@
+"use restrict"
+
+let n1 = 10
+
+//n1++
+
+//console.log(n1++) Pós incremento
+
+//console.log(++n1) Pré incremento
+
+
+
