36eff52abcbe39b4f3879bae11a25a0b655273c0
Marcos Casa <xmvcosta@gmail.com>
2023-05-14 01:36:44 -0300

    alterações feitas em casa 150520223 01:36

---
+++ cfbcursos/javascript/aula16/js/jscript.js
@@ -0,0 +1,32 @@
+"use restrict"
+
+// let num = [10,20,30,40,50]
+
+
+// //for
+// for(let i=0; i<num.length;i++){
+
+//     console.log(num[i])
+// }
+
+// //for in
+// for(n in num){ 
+//     console.log(num[n])
+// }
+
+// //for of
+// for(n of num){
+//     console.log(n)
+// }
+
+
+// const objs = document.getElementsByTagName("div")
+// //for in
+// for(o in objs){
+//     console.log(o)
+// }
+
+// //for of
+// for(o of objs){
+//     console.log(o)
+// }
---
+++ cfbcursos/javascript/aula17/js/jscript.js
@@ -0,0 +1,15 @@
+"use restrict"
+
+// let n=100
+
+// while(n>0){
+//     // console.log(n)
+
+//     if(n % 2 == 0){
+//         console.log(n + " é par")
+//     }else{
+//         console.log(n + " é impar")
+//     }
+
+//     n--
+// }
