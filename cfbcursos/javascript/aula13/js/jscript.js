d13c1b54e4a3a62a9ecea861a1972756ff993ba7
Marcos Casa <xmvcosta@gmail.com>
2023-05-13 22:58:15 -0300

    alteraçoes em casa 13052023 22:58

--- cfbcursos/javascript/aula13/js/jscript.js
+++ cfbcursos/javascript/aula13/js/jscript.js
@@ -1,2 +1,24 @@
 "use restrict"
 
+// let num1=40
+// if(num1>10){
+//     console.log("Número maior que 10.")
+//     if(num1 > 50){
+//         console.log("Número Maior que 50")
+//     }
+// }else if(num1 >5){
+//     console.log("Número entre 6 e 10")
+// }else{
+//     console.log("Número menor ou igual a 5")
+// }
+
+// console.log("Fim do programa")
+
+// let energia=100
+// let clima="chuva"
+
+// if(energia > 70 && clima == "sol"){
+//     console.log("Vou a praia!")
+// }else{
+//     console.log("Vou a academia")
+// }
