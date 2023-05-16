44ff1ac5cdb27a25c08ea7bf30e7f73b95c2e0c3
Marcos TTI <xmvcosta@gmail.com>
2023-05-16 17:39:26 -0300

    alterações feitas na tti 16052023 17:39h

--- cfbcursos/javascript/aula20/js/jscript.js
+++ cfbcursos/javascript/aula20/js/jscript.js
@@ -1,37 +1,7 @@
 "use restrict"
 
-// let n = 1
-// let max = 100
-
-// while(n<=max){
-//     console.log("Valor de n é : "+n)
-//     if(n>10){
-//         break
-//     }
-//     n++
-// }
-
-// while(n <= max){
-//     console.log("Valor de n é: " + n)
-//     if(n>10){
-//         console.log("O valor de n é: " + n)
-//         break
-//     }
-//     n++
-// }
-
-// for(let i=n;i<max;i++){
-//     console.log("Valor de n é: " + i)
-// }
-
-//let n=0
-let max=1000
-let pares=0
-for (let i = 0;i<max;i++){
-    console.log("Valor de i: "+i)
-    if(i%2!=0){
-        continue
-    }
-    pares++
+function nome(){
+    console.log("Marcos")
 }
-console.log(pares)
+
+nome()
