5432b73057a64b69d022a53fe8c14c0acd0ecf4e
Marcos Casa <xmvcosta@gmail.com>
2023-05-16 20:52:13 -0300

    Aula 23 javascript cfb cursos 16052023 20:52

--- cfbcursos/javascript/aula21/js/jscript.js
+++ cfbcursos/javascript/aula21/js/jscript.js
@@ -1,7 +1,22 @@
 "use restrict"
 
-function nome(){
-    console.log("Marcos")
+// function canal(){
+//     console.log("Canal")
+// }
+
+// canal()
+
+// for(i=0;i<10;i++){
+//     canal()
+// }
+
+function mat(){
+    let n1=10
+    let n2=2
+    let res=n1*n2
+    return res
 }
 
-nome()
+//console.log(mat())
+let num=mat()
+console.log(num)
---
+++ cfbcursos/javascript/aula23/js/jscript.js
@@ -0,0 +1,31 @@
+"use restrict"
+
+// function soma(n1,n2){
+//     console.log(n1 + n2)
+// }
+// soma(10,50)
+
+//valores default
+// function soma(n1=10,n2=3){
+//     console.log(n1 * n2)
+// }
+
+// soma()
+// soma(40)
+// soma(5,10)
+
+// function soma(n1,n2){
+//     return n1 + n2
+// }
+
+// console.log(soma(10,20))
+
+function add(v){
+    return  valor + v
+}
+
+let valor=0
+
+valor = add(15)
+
+console.log(valor)
