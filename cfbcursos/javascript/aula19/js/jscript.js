c01796428f0683d6a2e3f32a378ddbc1e13eacc0
Marcos Casa <xmvcosta@gmail.com>
2023-05-16 15:27:05 -0300

    Aula 19 javascript cfb cursos

---
+++ cfbcursos/javascript/aula19/js/jscript.js
@@ -0,0 +1,25 @@
+"use restrict"
+
+let n = 1
+let max = 100
+
+// while(n<=max){
+//     console.log("Valor de n é : "+n)
+//     if(n>10){
+//         break
+//     }
+//     n++
+// }
+
+// while(n <= max){
+//     console.log("Valor de n é: " + n)
+//     if(n>10){
+//         console.log("O valor de n é: " + n)
+//         break
+//     }
+//     n++
+// }
+
+for(let i=n;i<max;i++){
+    console.log("Valor de n é: " + i)
+}
