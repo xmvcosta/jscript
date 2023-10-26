39af174db5da2f2901ef0e5c2db969d03e36535e
Marcos Casa <xmvcosta@gmail.com>
2023-10-25 21:14:48 -0300

    Aula 03 javascript prof-edsonmaia 25102023 21:14

--- prof-edsonmaia/aula01/js/javascript.js
+++ prof-edsonmaia/aula01/js/javascript.js
@@ -3,7 +3,7 @@
 // document.write("Olá mundo!");
 // alert("olá mundo!");
 
-//##########
+//
 
 //Aula 02
 //https://www.youtube.com/watch?v=qsqeHCVYkMU&ab_channel=ProfessorEdsonMaia
@@ -49,40 +49,42 @@
 //AULA 03 - Curso de JS Aula 03 Operadores e Operações no JavaScript
 //https://www.youtube.com/watch?v=6d6IV3SQnIM&ab_channel=ProfessorEdsonMaia
 
-let n1 = 10
-let n2 = 2
-let n3 = "2"
+// let n1 = 10
+// let n2 = 2
+// let n3 = "2"
 
-document.write(`N1 = ${n1}</br>`)
-document.write(`N2 = ${n2}</br>`)
-document.write(`N3 = ${n3}</br>`)
+// document.write(`N1 = ${n1}</br>`)
+// document.write(`N2 = ${n2}</br>`)
+// document.write(`N3 = ${n3}</br>`)
 
-//operadores matematicos
-console.log(n1 + n2) //adição
-console.log(n1 - n2) //subtração
-console.log(n1 * n2) //multiplicação
-console.log(n1 / n2) //divisão
-console.log(n1 % n2) //resto ou modulo
+// //operadores matematicos
+// console.log(n1 + n2) //adição
+// console.log(n1 - n2) //subtração
+// console.log(n1 * n2) //multiplicação
+// console.log(n1 / n2) //divisão
+// console.log(n1 % n2) //resto ou modulo
 
-//operadores de igualdade e diferença
-console.log(n2 == n3) //igual a
-console.log(n2 === n3) //identico a
-console.log(n2 != n3) //diferente de Nao=
-console.log(n2 !== n3) //Nao é identico
+// //operadores de igualdade e diferença
+// console.log(n2 == n3) //igual a
+// console.log(n2 === n3) //identico a
+// console.log(n2 != n3) //diferente de Nao=
+// console.log(n2 !== n3) //Nao é identico
 
-//operadores logicos
-let a = true
-let b = false
+// //operadores logicos
+// let a = true
+// let b = false
 
-console.log(a && b) //and ou "E"
-console.log(a || b) //or ou "OU"
-console.log(!a) //negação "NOT" inverte o conteudo se é verdadeiro vira falso se é falso vira verdadeiro
-console.log(!b) //negação "NOT" inverte o conteudo se é verdadeiro vira falso se é falso vira verdadeiro
+// console.log(a && b) //and ou "E"
+// console.log(a || b) //or ou "OU"
+// console.log(!a) //negação "NOT" inverte o conteudo se é verdadeiro vira falso se é falso vira verdadeiro
+// console.log(!b) //negação "NOT" inverte o conteudo se é verdadeiro vira falso se é falso vira verdadeiro
 
 //operadores de comparação
-console.log(n1 > n2)
-console.log(n1 < n2)
-console.log(n1 >= n2)
-console.log(n1 <= n2)
+// console.log(n1 > n2)
+// console.log(n1 < n2)
+// console.log(n1 >= n2)
+// console.log(n1 <= n2)
 
 
+//AULA 04 - Curso de JS Aula 04 Arrays, métodos, length e spreed operator
+//https://www.youtube.com/watch?v=XisY6rpfbiw&ab_channel=ProfessorEdsonMaia
