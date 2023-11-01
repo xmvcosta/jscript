edc983715f2da9a78d408353f1ad06b2e5564c81
Marcos TTI <xmvcosta@gmail.com>
2023-11-01 17:34:49 -0300

    Aula 06 exercicios javascript prof-edsonmaia 01112023 17:33 TTI

---
+++ prof-edsonmaia/js/javascript-exercicio-aula-06.js
@@ -0,0 +1,48 @@
+//exercicio 01
+let lutador = "Marcos O GRANDE!"
+let nacionalidade = "Brasileiro"
+let idade = 39
+let peso = 66.5
+let altura = 1.71
+document.write("<h1>Lutadores</h1>")
+document.write("Lutador: "+ lutador + '<br>')
+document.write("Nacionalidade: " + nacionalidade + '<br>')
+document.write('Peso: ' + peso + " Kg <br>" )
+document.write("Altura: " + altura + " m <hr>")
+
+//exercicio 02
+const dia = 1
+const mes = 11
+const ano = 2023
+const fraseDoDia = "Distribua sorrisos por aí, com certeza alguem sorrirá também"
+const autor = "Internet"
+
+document.write("<h2>Frase do dia</h2>")
+document.write(`${dia}/${mes}/${ano}<br>`)
+document.write(fraseDoDia+'<br>')
+document.write(autor+'<hr>')
+
+//exercicio 03
+document.write("<h2>Meses do ano</h2>")
+let mesesDoAno = ["Janeiro", "Fevereiro", "Março"]
+document.write(`${mesesDoAno[0]}, ${mesesDoAno[1]}, ${mesesDoAno[2]}<hr>`)
+
+//exercicio 04
+const jogo = {
+    nome: "League of Legends",
+    desenvolvedor: "Riot Games",
+    anoLancamento: 2009
+}
+document.write("<h2>Jogo</h2>")
+document.write(`Jogo: ${jogo.nome}<br> Desenvolvedor: ${jogo.desenvolvedor}<br> Lançamento: 
+${jogo.anoLancamento}<hr>`)
+
+
+
+
+
+
+
+
+
+
